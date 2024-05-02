3c7df3512c80ce3f8b4b59fa3b42e66c2c85571e
murilojcavalcanti <josemurilo7@gmail.com>
2024-05-02 11:23:12 -0300

    Adicionar arquivos de projeto.

---
+++ CleanArchitecture.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        //Esse tipo é uma estrutura que representa uma data e hora, além do deslocamento para UTC.
+        public DateTimeOffset DateCreated { get; set; }
+        public DateTimeOffset? DateUpdated { get; set; }
+        public DateTimeOffset? DateDeleted { get; set; }
+    }
+}
