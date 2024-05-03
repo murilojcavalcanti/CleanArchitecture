973a3534d90a39dfa4e147ba3c5458b40307053f
murilojcavalcanti <josemurilo7@gmail.com>
2024-05-03 08:58:50 -0300

    implementando Classe Domain

---
+++ CleanArchitecture.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface IBaseRepository <T> where T : BaseEntity
+    {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void GetAll(Guid id,CancellationToken cancellationToken);
+        void GetById(CancellationToken cancellationToken);
+    }
+}
